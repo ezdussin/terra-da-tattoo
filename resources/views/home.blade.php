6f6e20a5e1f6354c348372da1ed06e8ccb835bd9
Luciana Dussin <enzodussinn@gmail.com>
2022-10-26 18:14:31 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <title>Terra da Tattoo</title>
+</head>
+<body>
+    <header>
+        <h1>Terra da Tattoo</h1>
+    </header>
+    <div class="content">
+        <div class="buttons-container">
+            <button>USUÁRIOS</button>
+            <button>FORNECEDORES</button>
+            <button>PRODUTOS</button>
+            <button>MAIS</button>
+        </div>
+    </div>
+</body>
+</html>
